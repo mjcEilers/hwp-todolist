64f31ea0c97cfebf811e7f7e7cb6546665a2082c
Daza3s <joawagner15@gmail.com>
2025-06-22 16:38:11 +0200

    aufgabe 5

--- ex01/displayTest/displayTest.ino
+++ ex01/displayTest/displayTest.ino
@@ -30,19 +30,65 @@ void setup() {
   lcd.print("button: -");
 }
 
+int getButtonI(int inp) {
+  // Button 1: 0 
+  // Button 2: 242
+  // Button 3: 473
+  // Button 4: 677
+  // Button 5: 830
+  if (inp < 121) {
+    return 1;
+  }else if(inp < 357) {
+    return 2;
+  }else if(inp < 575) {
+    return 3;
+  }else if(inp < 753) {
+    return 4;
+  }else if(inp < 926) {
+    return 5;
+  }else {
+    return 0;
+  }
+}
+
+const char* getButtonC(int inp) {
+  // Button 1: 0 
+  // Button 2: 242
+  // Button 3: 473
+  // Button 4: 677
+  // Button 5: 830
+  if (inp < 121) {
+    return "S1";
+  }else if(inp < 357) {
+    return "S2";
+  }else if(inp < 575) {
+    return "S3";
+  }else if(inp < 753) {
+    return "S4";
+  }else if(inp < 926) {
+    return "S5";
+  }else {
+    return "- ";
+  }
+}
+
 void loop() {
   // Set cursor to arbitrary position
-  lcd.setCursor(10, 0);
+  lcd.setCursor(10,0);
 
   // Read input
   int a0_read = analogRead(A0);
   float voltage = a0_read * REFERENCE_VOLTAGE / MAX_READ;
   char digits[4];
   dtostrf(voltage, 4, 2, digits);
-  // Print statements
+  // Print row 0
   lcd.print(digits);
+  
+  lcd.setCursor(8,1);
+  int a1_read = analogRead(A1);
+  // Print row 1
+  lcd.print((char[2])getButtonC(a1_read));
   // Prevent display flickering for too fast updates
-  lcd
   delay(100);
   // NOTE: If you update only parts of the screen, don't use lcd.clear.
   // Set the cursor to the line and column to be updated and override existing chars.
