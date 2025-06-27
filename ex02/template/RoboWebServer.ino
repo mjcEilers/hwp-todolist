4a27f680a3d34793262d080b30b4fef499bdf436
Max Eilers <100074785+mjcEilers@users.noreply.github.com>
2025-06-27 09:07:09 +0200

    Add ex02 template

---
+++ ex02/template/RoboWebServer.ino
@@ -0,0 +1,139 @@
+// Include WiFi libs
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include "website.h"
+
+// Add your wifi credentials here
+const char* ssid     = "SecretAP";
+const char* password = "MySuperSecretPassword";
+
+// Webserver on port 80 (standard http port)
+WiFiServer server(80);
+
+// Variable to store incoiming http request
+String request;
+
+// Name of the device (can be used as DNS query in browser)
+#define DEVICE_NAME "HWPRobo"
+
+// Pins of motor
+#define MOTOR_A1_PIN D1
+#define MOTOR_A2_PIN D2
+#define MOTOR_B1_PIN D5
+#define MOTOR_B2_PIN D6
+const uint8_t motorPins[] = {MOTOR_A1_PIN, MOTOR_A2_PIN, MOTOR_B1_PIN, MOTOR_B2_PIN};
+
+// Us pins
+#define US1_PIN D8
+#define US2_PIN D7
+#define US3_PIN D3
+const uint8_t usPins[] = {US1_PIN, US2_PIN, US3_PIN};
+
+
+void setup() {
+  // Init serial
+  Serial.begin(115200);
+
+  // Init motor pins as output
+  for (size_t i = 0; i < sizeof(motorPins)/sizeof(motorPins[0]); i++) {
+    pinMode(motorPins[i], OUTPUT);
+    digitalWrite(motorPins[i], LOW);
+  }
+  
+  // Comment if you want that the ESP creates an AP
+  // Connect to wifi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Die IP vom Webserver auf dem seriellen Monitor ausgeben
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP Adress: ");
+  Serial.println(WiFi.localIP());
+
+  // Uncomment if you want that the ESP creates an AP
+  /*
+  // You can remove the password parameter if you want the AP to be open. 
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  */
+
+  // mDNS name resolving
+  if (MDNS.begin(DEVICE_NAME)) {
+    Serial.println("MDNS started");
+  } else {
+    Serial.println("Error starting MDNS");
+  }
+
+  // Start webserver
+  server.begin();
+}
+
+void loop() {
+  // Handle clients
+  handleClient();
+  // Update MDNS
+  MDNS.update();
+}
+
+void handleClient() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client)  {  
+    return;  
+  }
+  // Read the first line of the request (we just need this line)
+  request = client.readStringUntil('\r');
+
+  // Print request to serial
+  Serial.print("request: ");
+  Serial.println(request); 
+
+  // print header message
+  client.println(header);
+  // Check for corresponding get message  
+  if (request.indexOf("GET /pollUS") >= 0) {
+    Serial.println("Polling");
+    float us1, us2, us3 = -1;
+    // Insert your code here
+
+
+    // Send US data to website
+    client.printf("{\"US1\":%.2f, \"US2\":%.2f, \"US3\":%.2f}", us1, us2, us3);
+    
+  // Insert code to make the d-pad control working
+  // Start by pressing the buttons of the d pad and watch the serial console to see how the get requests look.
+
+  
+  // Serve initial Website
+  } else {
+    // Finish HTTP-Request with a newline (thats cruical)
+    client.flush();
+    client.println(page);
+    client.println();
+  }
+}
+
+
+float measureDistance(uint8_t pin) {
+  // To implement
+  return -1;  
+}
+
+void turn(bool left, uint16_t time, uint16_t speed) {
+  // To implement
+}
+
+void drive(bool forward, uint16_t time, uint16_t speed) {
+  // To implement
+}
+
+void setMotor(bool forward, bool motorA, uint16_t speed) {
+  // To implement
+}
