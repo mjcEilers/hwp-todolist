98254fef75edb3aa70deefc20cd5e80831b031ab
LisaSophieAndrina <lisoan.schmidt@gmail.com>
2025-07-06 15:39:57 +0200

    Add files via upload

---
+++ ex3/Aufgaben/Aufgaben.ino
@@ -0,0 +1,42 @@
+#define SERIAL_SPEED 115200
+
+void setup() {
+  Serial.beginn(SERIAL_SPEED);
+  DDRB |= (1 << PB5); //defines Pin 13 as output
+  //PORTB is register, Pin 13 is fifth bit of PORTB
+  PORTB |= (1 << PB5); //initialises PORTB
+  DDRB |= (1 << PB4); //defines Pin 12 as output
+}
+
+void setPin13(bool high){
+  if (high == True){
+    PORTB |= (1 << PB5);
+  }
+  else{
+    PORTB &= ~(1 << PB5);
+  }
+}
+
+void setTimer1Freq(){
+  cli();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TIMSK1 = 0;
+  TCCR1B |= (1 << CS12)|(1 << CS11)|(1 << CS10);
+  TCCR1A |= (1 << WGM11);
+  OCR1A = x;
+  TIMSK1 |= (1 << OCIE1A);
+  sei();
+}
+
+ISR(TIMER1_COMPA_vect){
+  Serial.println("Heurika");
+  PINB |= (PB4);
+}
+
+void loop() {
+  setPin13(True);
+  delay(500);
+  setPin13(False);
+  delay(500);
+}
