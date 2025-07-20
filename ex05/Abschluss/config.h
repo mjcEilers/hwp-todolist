99851be54fb00ab9ea157eb8cf8519192077a3e6
Max Eilers <100074785+mjcEilers@users.noreply.github.com>
2025-07-20 19:43:52 +0200

    [ex05] sketched config.h

---
+++ ex05/Abschluss/config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Motor pins
+#define MOTOR_A1_PIN 00
+#define MOTOR_A2_PIN 00
+#define MOTOR_B1_PIN 00
+#define MOTOR_B2_PIN 00
+
+// Ultrasonic pins
+#define US1_PIN 00
+#define US2_PIN 00
+#define US3_PIN 00
+
+// Button & Trimmer pins
+#define TR1_PIN 00
+#define TR2_PIN 00
+#define BUTTONS_PIN 00
+
+#endif
