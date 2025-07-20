fcc988920bb3016e7c79a768b3cb88b4116302ab
Max Eilers <100074785+mjcEilers@users.noreply.github.com>
2025-07-20 23:04:27 +0200

    [ex05] update config.h

--- ex05/Abschluss/config.h
+++ ex05/Abschluss/config.h
@@ -12,9 +12,21 @@
 #define US2_PIN 00
 #define US3_PIN 00
 
-// Button & Trimmer pins
+// LCD pins
+#define LCD_RS 11
+#define LCD_E 12
+#define LCD_DB4 13
+#define LCD_DB5 A0
+#define LCD_DB6 A1
+#define LCD_DB7 A2
+
+#define NUM_CHARS 20
+#define NUM_LINES 4
+
+// Button, Trimmer & Gyro pins
 #define TR1_PIN 00
 #define TR2_PIN 00
 #define BUTTONS_PIN 00
+#define GYRO_PIN A3
 
 #endif
