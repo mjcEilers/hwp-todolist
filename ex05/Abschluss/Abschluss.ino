fe84a99b3df1d4ef30a90727ee88d3c042e12e46
Max Eilers <100074785+mjcEilers@users.noreply.github.com>
2025-07-20 23:20:09 +0200

    [ex05] add minimal entrypoint

--- ex05/Abschluss/Abschluss.ino
+++ ex05/Abschluss/Abschluss.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "config.h"
+#include "Gyro.h"
+#include <LiquidCrystal.h>
 
+Gyro gyro(GYRO_PIN);
+LiquidCrystal lcd(LCD_RS, LCD_E, LCD_DB4, LCD_DB5, LCD_DB6, LCD_DB7);
+
+void setup() {
+  gyro.begin();
+  lcd.begin(NUM_CHARS, NUM_LINES);
+  lcd.setCursor(0, 0);
+  lcd.print("Heading: ");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  gyro.update();
 
+  float angle = gyro.getAngle();
+  lcd.setCursor(9, 0);
+  lcd.print(angle);
+  delay(100);
 }
