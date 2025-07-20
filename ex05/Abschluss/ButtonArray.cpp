9cf389a26e053eeb48a8ad1fce3094ed19d9a369
Max Eilers <100074785+mjcEilers@users.noreply.github.com>
2025-07-20 20:31:01 +0200

    [ex05] Implemented ButtonArray

---
+++ ex05/Abschluss/ButtonArray.cpp
@@ -0,0 +1,37 @@
+#include "ButtonArray.h"
+
+// Constructor
+ButtonArray::ButtonArray(uint8_t inputPin, uint16_t threshold, uint16_t debounceDelay)
+  : _pin(inputPin), _threshold(threshold), _debouceDelay(debounceDelay),
+    _current(NONE), _lastStable(NONE), lastChangeTime(0) {}
+
+void ButtonArray::update() {
+  uint16_t analogValue = analogRead(_pin);
+  Button reading = detectButton(analogValue);
+
+  if (reading != _current) {
+    _current = reading;
+    _lastChangeTime = millis();
+  }
+
+  if ((millis() - lastChangeTime) > _debounceDelay && reading != _lastStable) {
+    _lastStable = reading;
+  }
+}
+
+bool ButtonArray::isPressed(Button button) {
+  return button == _lastStable;
+}
+
+ButtonArray::Button ButtonArray::getPressedButton() {
+  return _lastStable;
+}
+
+ButtonArray::Button ButtonArray::detectButton(uint16_t value) {
+  if (abs(value -   0) < _threshold) return S1;
+  if (abs(value - 242) < _threshold) return S2;
+  if (abs(value - 473) < _threshold) return S3;
+  if (abs(value - 677) < _threshold) return S4;
+  if (abs(value - 830) < _threshold) return S5;
+  return NONE;
+}
---
+++ ex05/Abschluss/ButtonArray.h
@@ -0,0 +1,36 @@
+#ifndef BUTTON_ARRAY_H
+#define BUTTON_ARRAY_H
+
+#include <Arduino.h>
+
+class ButtonArray {
+  int inputPin;
+
+public:
+  enum Button {
+    NONE,
+    S1,
+    S2,
+    S3,
+    S4,
+    S5
+  };
+
+  ButtonArray(uint8_t inputPin, uint16_t threshold, uint16_t debounceDelay);
+  void update();
+  bool isPressed(Button button);
+  Button getPressedButton();
+
+private:
+  uint8_t _pin;
+  uint16_t _threshold;  // Noise threshold
+  uint16_t _debounceDelay;
+
+  Button _current;
+  Button _lastStable;
+  unsigned long _lastChangeTime;
+
+  Button detectButton(uint16_t value);
+};
+
+#endif
