5439ace65468f13035e8fffaf9bac1a8e1d53a95
Max Eilers <100074785+mjcEilers@users.noreply.github.com>
2025-07-21 17:23:25 +0200

    [ex05] updated pins

--- ex05/Abschluss/config.h
+++ ex05/Abschluss/config.h
@@ -3,31 +3,31 @@
 
 #define COMSPEED 9600
 // Motor pins
-#define MOTOR_A1_PIN 00
-#define MOTOR_A2_PIN 00
-#define MOTOR_B1_PIN 00
-#define MOTOR_B2_PIN 00
+#define MOTOR_A1_PIN 5
+#define MOTOR_A2_PIN 6
+#define MOTOR_B1_PIN 9
+#define MOTOR_B2_PIN 10
 
 // Ultrasonic pins
-#define US1_PIN 00
-#define US2_PIN 00
-#define US3_PIN 00
+#define US1_PIN 3
+#define US2_PIN 4
+#define US3_PIN 5
 
 // LCD pins
-#define LCD_RS 11
-#define LCD_E 12
-#define LCD_DB4 13
-#define LCD_DB5 A0
-#define LCD_DB6 A1
-#define LCD_DB7 A2
+#define LCD_RS 2
+#define LCD_E 3
+#define LCD_DB4 4
+#define LCD_DB5 5
+#define LCD_DB6 6
+#define LCD_DB7 7
 
 #define NUM_CHARS 20
 #define NUM_LINES 4
 
 // Button, Trimmer & Gyro pins
-#define TR1_PIN 00
+#define TR1_PIN A0
 #define TR2_PIN 00
-#define BUTTONS_PIN 00
-#define GYRO_PIN A3
+#define BUTTONS_PIN A1
+#define GYRO_PIN A2
 
 #endif
